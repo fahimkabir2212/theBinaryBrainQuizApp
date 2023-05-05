af7f3da11bc8cc1e406518d83be7803e090b2ace
A K M Fahim Kabir <fahimkabir2213@gmail.com>
2023-05-05 16:12:06 +0600

    Changed the whole layout of login and Registration page design, backend yet to code.

--- app/src/main/java/com/example/thebinarybrain_quizapp/Login.java
+++ app/src/main/java/com/example/thebinarybrain_quizapp/Login.java
@@ -18,70 +18,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-
-    TextInputEditText editTextEmail,editTextPassword;
-    Button signIn;
-    TextView signUp;
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        editTextEmail = findViewById(R.id.email);
-        editTextPassword = findViewById(R.id.password);
-        signIn = findViewById(R.id.signIn);
-        signUp = findViewById(R.id.signUp);
-
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this,Registration.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email,password;
-                email = String.valueOf(editTextEmail.getText());
-                password = String.valueOf(editTextPassword.getText());
-
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(Login.this, "Enter your email", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(password)){
-                    Toast.makeText(Login.this, "Enter Password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                firebaseAuth.signInWithEmailAndPassword(email,password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-
-                                    if (firebaseAuth.getCurrentUser().isEmailVerified()){
-                                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                        Intent intent= new Intent(Login.this,HomePage.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                    else {
-                                        Toast.makeText(Login.this, "Please varify your email", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                                else {
-                                    Toast.makeText(Login.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
 
     }
 }
--- app/src/main/java/com/example/thebinarybrain_quizapp/Registration.java
+++ app/src/main/java/com/example/thebinarybrain_quizapp/Registration.java
@@ -19,74 +19,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
 
-
-    TextInputEditText editTextEmail,editTextPassword;
-    Button signUp;
-    TextView signIn;
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
-        editTextEmail = findViewById(R.id.email);
-        editTextPassword = findViewById(R.id.password);
-        signIn = findViewById(R.id.signIn);
-        signUp = findViewById(R.id.signUp);
-
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Registration.this,Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email,password;
-                email = String.valueOf(editTextEmail.getText());
-                password = String.valueOf(editTextPassword.getText());
-
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(Registration.this, "Enter your email", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(password)){
-                    Toast.makeText(Registration.this, "Enter Password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                firebaseAuth.createUserWithEmailAndPassword(email,password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-
-                                    firebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if(task.isSuccessful()){
-                                                Toast.makeText(Registration.this, "User registration is Successful, please varify your email", Toast.LENGTH_SHORT).show();
-                                                Intent intent = new Intent(Registration.this,Login.class);
-                                                startActivity(intent);
-                                                finish();
-                                            }
-                                            else {
-                                                Toast.makeText(Registration.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
-                                }
-                                else {
-                                    Toast.makeText(Registration.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
     }
 }
